35b745d03b9929e3420d410881edcc789b76b525
Creban <p--p@nate.com>
2017-04-28 23:00:27 +0900

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,6 @@
 class Helloworld{
   public static void main(String[] args){
-    System.out.println("test Hello World");
+    System.out.println("Test Hello World");
+    System.out.println("Test Hello World");
   }
 }  
