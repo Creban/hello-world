b796ef261d553ebcefd8907a21b7173a1e5670b9
K2H <K2H@K2H-PC>
2017-09-29 21:58:13 +0900

    TEST add plus function

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,10 @@
 class Helloworld{
   public static void main(String[] args){
     System.out.println("test Hello World");
+    System.out.println(plus(4,3));
   }
+
+public static int plus(int a, int b){
+	  return a+b;
+}
 }  
